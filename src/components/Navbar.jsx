fb55ad7536ebec239d90e25cded46ec93329fa56
Vichara Tharkana <vichara0115@gmail.com>
2026-05-08 12:00:27 +0530

    NavBar With Nav Links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import { NAV_LINKS } from '../data'
+
+export default function Navbar() {
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,11 @@
+
+
+export const NAV_LINKS = [
+  { label: "About", href: "#about" },
+  { label: "Skills", href: "#skills" },
+  { label: "Projects", href: "#projects" },
+  { label: "Experience", href: "#experience" },
+  { label: "Education", href: "#education" },
+  { label: "Contact", href: "#contact" },
+];
+
